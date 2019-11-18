49362071cb1867af88329418cdc0d7b39747a8e2
Edward Rochester <e.rochester@ucalgary.ca>
2019-11-18 11:25:04 -0700

    Tutorial-16 Exercise Solution Added

---
+++ Tutorial_16/example.s
@@ -0,0 +1,38 @@
+		.data
+x_m:	.word	0
+j_m:	.word	2
+i_m:	.word	4
+
+		.text
+
+fmt:	.string	"Sum is: %d\n" 		//string literal
+
+		.balign	4
+		.global	main
+main:	stp		x29, x30, [sp, -16]!
+		mov		x29, sp
+
+		adrp	x19, j_m
+		add		x19, x19, :lo12:j_m
+		ldr		w20, [x19]			// w20 = j
+
+		adrp	x19, i_m
+		add		x19, x19, :lo12:i_m
+		ldr		w21, [x19]			// w20 = i
+
+		add		w22, w20, w21
+
+		adrp	x19, x_m
+		add		x19, x19, :lo12:x_m
+		str		w22, [x19]			// w20 = k
+
+		adrp	x0, fmt
+		add		x0, x0, :lo12:fmt 	//address of "S"
+		ldr		x1, [x19]			// now in x0
+		bl 		printf
+
+		mov		w0, 0
+
+		ldp		x29, x30, [sp], 16
+		ret
+
