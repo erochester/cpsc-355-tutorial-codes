e7a7fce45d8d597c4f14154551d4208a16ca0d4b
Edward Rochester <e.rochester@ucalgary.ca>
2019-11-25 17:22:24 -0700

    Update solution.s

--- Tutorial_18/solution.s
+++ Tutorial_18/solution.s
@@ -1,31 +1,31 @@
 		.text
-fmt:	.string	"Total: %d\n"
+fmt:		.string	"Total: %d\n"
 
 		.balign	4
 		.global main
-main:	stp		x29, x30, [sp, -16]!
-		mov		x29, sp
+main:		stp	x29, x30, [sp, -16]!
+		mov	x29, sp
 
-		mov		w21, w0 				// argc
-		mov		x22, x1 				// argv
-		mov		w20, 0 					// sum
+		mov	w21, w0 				// argc
+		mov	x22, x1 				// argv
+		mov	w20, 0 					// sum
 
-		mov		w19, 1 					// i = 1 (first argument is name of the program)
-		b 		test					// branch to test
+		mov	w19, 1 					// i = 1 (first argument is name of the program)
+		b 	test					// branch to test
 
-top: 	ldr		x0, [x22, w19, sxtw 3]	// load the address of the string entered
-		bl 		atoi					// call atoi()
-		add		w20, w20, w0 			// add to sum
+top: 		ldr	x0, [x22, w19, sxtw 3]			// load the address of the string entered
+		bl 	atoi					// call atoi()
+		add	w20, w20, w0 				// add to sum
 		add 	w19, w19, 1				// increment i
 
-test:	cmp		w19, w21				// i < argc
-		b.lt	top						// if True, branch to top
+test:		cmp	w19, w21				// i < argc
+		b.lt	top					// if True, branch to top
 
 		adrp 	x0, fmt 				// print sum
 		add 	x0, x0, :lo12:fmt
 		mov 	w1, w20
-		bl 		printf
+		bl 	printf
 
-		mov		w0, 0
+		mov	w0, 0
 		ldp 	x29, x30, [sp], 16
 		ret
