0e3fed23593b96709d7125bf00a31818bc14aacb
Edward Rochester <e.rochester@ucalgary.ca>
2019-12-02 16:32:06 -0700

    Update example2.asm

--- Tutorial_20/example2.asm
+++ Tutorial_20/example2.asm
@@ -2,10 +2,10 @@ define(fd, w19)
 define(x_r, d8)
 define(sum_r, d9)
 
-fmt0: 	.string "Error opening a file. Aborting\n"
-fmt1: 	.string "Sum is: %13.10f\n"
+fmt0: 		.string "Error opening a file. Aborting\n"
+fmt1: 		.string "Sum is: %13.10f\n"
 
-pn: 	.string "output.bin"
+pn: 		.string "output.bin"
 
 		AT_FDCWD = -100
 		buf_size = 8
@@ -16,20 +16,20 @@ pn: 	.string "output.bin"
 		.balign 4
 		.global main
 
-main: 	stp x29, x30, [sp, alloc]!
+main: 		stp x29, x30, [sp, alloc]!
 		mov x29, sp
 
 		mov w0, AT_FDCWD		// 1st arg (current working directory)
 		adrp x1, pn
-		add x1, x1, :lo12:pn 	// 2nd arg (pathname)
-		mov w2, 0				// 3rd arg (read-only)
-		mov w3, 0				// 4th arg (not used)
-		mov x8, 56				// openat I/O request
-		svc 0					// call system function
-		mov fd, w0				// record the file descriptor
+		add x1, x1, :lo12:pn 		// 2nd arg (pathname)
+		mov w2, 0			// 3rd arg (read-only)
+		mov w3, 0			// 4th arg (not used)
+		mov x8, 56			// openat I/O request
+		svc 0				// call system function
+		mov fd, w0			// record the file descriptor
 
-		cmp fd, 0				// check if file opened normally
-		b.ge openok				// if yes we go to openok
+		cmp fd, 0			// check if file opened normally
+		b.ge openok			// if yes we go to openok
 
 		adrp x0, fmt0
 		add x0, x0, :lo12:fmt0
@@ -37,24 +37,24 @@ main: 	stp x29, x30, [sp, alloc]!
 		mov w0, -1
 		b exit
 
-openok: mov w0, fd 				// 1st arg (file descriptor)
+openok: 	mov w0, fd 			// 1st arg (file descriptor)
 		add x1, x29, buf_s 		// 2nd erg (pointer to buf)
 		mov x2, buf_size 		// 3rd arg (n bytes to read)
-		mov x8, 63 				// read service request
-		svc 0 					// system call
+		mov x8, 63 			// read service request
+		svc 0 				// system call
 
 		cmp w0, buf_size 		// check that n_read > 8
-		b.lt end 				// if less than end of the file
+		b.lt end 			// if less than end of the file
 
-		ldr x_r, [x29, buf_s] 	// store double into the register
+		ldr x_r, [x29, buf_s] 		// store double into the register
 
-		fadd sum_r, sum_r, x_r 	// add the double to the sum
+		fadd sum_r, sum_r, x_r 		// add the double to the sum
 
-		b openok 				// loop back again
+		b openok 			// loop back again
 
-end: 	mov w0, fd 				// 1st arg (file descriptor)
-		mov x8, 57 				// close() service request
-		svc 0 					// system call
+end: 		mov w0, fd 			// 1st arg (file descriptor)
+		mov x8, 57 			// close() service request
+		svc 0 				// system call
 
 		adrp x0, fmt1
 		add x0, x0, :lo12:fmt1
@@ -63,7 +63,7 @@ end: 	mov w0, fd 				// 1st arg (file descriptor)
 
 		mov w0, 0
 
-exit: 	ldp x29, x30, [sp], dealloc
+exit: 		ldp x29, x30, [sp], dealloc
 		ret
 
 		
