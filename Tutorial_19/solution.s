c3ae7306974b3ca775f6e62c8b9e8810e366b7ed
Edward Rochester <e.rochester@ucalgary.ca>
2019-12-02 16:29:38 -0700

    Update solution.s

--- Tutorial_19/solution.s
+++ Tutorial_19/solution.s
@@ -1,9 +1,9 @@
 		.text
-fmt:	.string	"Total: %d\n"
+fmt:		.string	"Total: %d\n"
 
 		.balign	4
 		.global main
-main:	stp		x29, x30, [sp, -16]!
+main:		stp		x29, x30, [sp, -16]!
 		mov		x29, sp
 
 		mov		w21, w0 				// argc
@@ -13,19 +13,19 @@ main:	stp		x29, x30, [sp, -16]!
 		mov		w19, 1 					// i = 1 (first argument is name of the program)
 		b 		test					// branch to test
 
-top: 	ldr		x0, [x22, w19, sxtw 3]	// load the address of the string entered
+top: 		ldr		x0, [x22, w19, sxtw 3]			// load the address of the string entered
 		bl 		atoi					// call atoi()
-		add		w20, w20, w0 			// add to sum
-		add 	w19, w19, 1				// increment i
+		add		w20, w20, w0 				// add to sum
+		add 		w19, w19, 1				// increment i
 
-test:	cmp		w19, w21				// i < argc
-		b.lt	top						// if True, branch to top
+test:		cmp		w19, w21				// i < argc
+		b.lt		top					// if True, branch to top
 
-		adrp 	x0, fmt 				// print sum
-		add 	x0, x0, :lo12:fmt
-		mov 	w1, w20
+		adrp 		x0, fmt 				// print sum
+		add 		x0, x0, :lo12:fmt
+		mov 		w1, w20
 		bl 		printf
 
 		mov		w0, 0
-		ldp 	x29, x30, [sp], 16
+		ldp 		x29, x30, [sp], 16
 		ret
